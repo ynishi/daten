4f1ae70222a453dc18aed52f0fba4abc68eff34e
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-09-08 01:57:24 +0900

    1st commit

---
+++ src/Daten.hs
@@ -0,0 +1,30 @@
+module Daten 
+  (
+  DatenDate,
+  DatenFormat,
+  Daten(..),
+  toFormatteds,
+  join
+  ) where
+
+import Data.Time.Calendar
+import Data.Time.Format
+import Data.List
+
+type DatenDate = (Integer, Int, Int)
+type DatenFormat = String
+
+data Daten = Daten DatenDate DatenDate DatenFormat deriving(Show)
+
+toFormatteds d@(Daten start end tpl) = map showWithFormat $ dates start end 
+  where
+    showWithFormat = formatTime defaultTimeLocale tpl
+
+dates (sy, sm, sd) (ey, em, ed) = takeWhile (<= endDay) . map addDaysToStart $ [0..]
+  where
+    startDay = fromGregorian sy sm sd
+    endDay = fromGregorian ey em ed 
+    addDays' = flip addDays
+    addDaysToStart = addDays' startDay
+
+join x d = "'" ++ (intercalate x $ toFormatteds d) ++ "'"
---
+++ test/DatenSpec.hs
@@ -0,0 +1,32 @@
+module DatenSpec
+  ( spec
+  ) where
+
+import           Daten
+import           Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "toFormatteds" $ do
+    it "creates formattted list" $ do
+      let cases =
+            [ ( ((2019, 1, 1), (2019, 1, 2), "%Y-%m-%d")
+              , ["2019-01-01", "2019-01-02"])
+            , ( ((2019, 1, 1), (2019, 1, 3), "%Y-%m-%d")
+              , ["2019-01-01", "2019-01-02", "2019-01-03"])
+            , (((2019, 1, 1), (2019, 1, 2), "%Y%m%d"), ["20190101", "20190102"])
+            ]
+      mapM_
+        (\((x1, x2, x3), expected) ->
+           toFormatteds (Daten x1 x2 x3) `shouldBe` expected)
+        cases
+  describe "join" $ do
+    it "creates joined String" $ do
+      let cases =
+            [ ( ("',\n'", (2019, 1, 1), (2019, 1, 2), "%Y-%m-%d")
+              , "'2019-01-01',\n'2019-01-02'")
+              ]
+      mapM_
+        (\((x1, x2, x3, x4), expected) ->
+           join x1 (Daten x2 x3 x4) `shouldBe` expected)
+        cases
